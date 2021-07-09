ea893a739880b33e12d375a9efcc89736c15a169
Fawad Ali <fawad.ali@mercateo.com>
2021-07-09 13:45:46 +0200

    Create a naive loader for removing React props

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,13 @@ export type LoaderOptions = {
 
 function loader(this: LoaderContext<LoaderOptions>, contents: string) {
   const options = this.getOptions();
-  
+	const props = options.props || []
+
+	props.forEach(prop => {
+		contents = contents.replace(new RegExp(`${prop}=("|{).*?("|})(?=\\s|>)`, "g"), "")
+	})
+
+	return contents
 }
 
 export default loader;
