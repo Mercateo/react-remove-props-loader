f4824adde3ea217b4c3f9e564979d277f45d6b9b
Fawad Ali <m.fawaadali98@gmail.com>
2021-07-15 21:50:45 +0500

    Improve tests

--- tests/loader.test.ts
+++ tests/loader.test.ts
@@ -1,19 +1,21 @@
+import { LoaderContext } from "webpack";
 import loader, { LoaderOptions, ScriptKind } from "../src";
 
 describe("react-remove-props-loader", () => {
-  const mockLoaderThis = (options: LoaderOptions): any => ({
-    getOptions: () => options,
-    resourcePath: "test/file.ts",
-  });
-  const code = `const TestComponent = () => (
-    <div test-attribute="test-value" test-attribute-2={testVar} test-attribute-not-removed="test-value">Some JSX Element</div>
-  );`;
+  const mockLoaderThis = (options: LoaderOptions) =>
+    ({
+      getOptions: () => options,
+      resourcePath: "test/file",
+    } as LoaderContext<LoaderOptions>);
 
   it("should remove specified attributes", () => {
+    const code = `const TestComponent = () => (
+      <div test-attribute="test-value" test-attribute-2={testVar} test-attribute-not-removed="test-value">Some JSX Element</div>
+    );`;
     const options: LoaderOptions = {
       props: ["test-attribute", "test-attribute-2"],
     };
-    const loaderThis: any = mockLoaderThis(options);
+    const loaderThis = mockLoaderThis(options);
 
     const transpiledCode = loader.bind(loaderThis)(code);
     const expectedCode = code.replace(
@@ -24,13 +26,30 @@ describe("react-remove-props-loader", () => {
   });
 
   it("should return content as is when no props in options", () => {
+    const code = `const TestComponent = () => (
+      <div test-attribute="test-value">Some JSX Element</div>
+    );`;
     const options: LoaderOptions = {
       props: [],
-      scriptKind: ScriptKind.JSX,
     };
-    const loaderThis: any = mockLoaderThis(options);
+    const loaderThis = mockLoaderThis(options);
 
     const transpiledCode = loader.bind(loaderThis)(code);
     expect(transpiledCode).toBe(code);
+  });
+
+  it("should transpile code based on the specified script kind", () => {
+    const code = `const TestComponent: FC = () => (
+      <div test-attribute="test-value">Some JSX Element</div>
+    );`;
+    const options: LoaderOptions = {
+      props: ["test-attribute"],
+      scriptKind: ScriptKind.TSX,
+    };
+    const loaderThis = mockLoaderThis(options);
+
+    const transpiledCode = loader.bind(loaderThis)(code);
+    const expectedCode = code.replace(` test-attribute="test-value"`, "");
+    expect(transpiledCode).toBe(expectedCode);
   });
 });
